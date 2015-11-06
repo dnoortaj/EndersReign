885696029ee071e2a733746156c67815eb61c0a2
Danyelle <Danyelle@10.251.15.94>
2015-11-06 11:41:57 -0500

    adding Room to repository (still working on it)

---
+++ src/Obstacle/Room.java
@@ -0,0 +1,93 @@
+package Obstacle;
+
+import java.util.ArrayList;
+
+public class Room
+{
+	private String roomDescription;
+	private int roomMonster;
+	private int roomMonsterChance;
+	private int roomPuzzle;
+	private int roomPuzzleChance;
+	private ArrayList<Room> roomExits;
+	
+	public Room()
+	{
+		roomDescription = "";
+		roomMonster = 0;
+		roomMonsterChance = 0;
+		roomPuzzle = 0;
+		roomPuzzleChance = 0;
+		roomExits = new ArrayList<Room>();
+	}
+	
+	public void look()
+	{
+		
+	}
+	
+	public void determineEncounter()
+	{
+		
+	}
+	
+	public void setRoomDescription(String roomDescription)
+	{
+		this.roomDescription = roomDescription;
+	}
+
+	public void setRoomMonster(int roomMonster)
+	{
+		this.roomMonster = roomMonster;
+	}
+
+	public void setRoomMonsterChance(int roomMonsterChance)
+	{
+		this.roomMonsterChance = roomMonsterChance;
+	}
+
+	public void setRoomPuzzle(int roomPuzzle)
+	{
+		this.roomPuzzle = roomPuzzle;
+	}
+
+	public void setRoomPuzzleChance(int roomPuzzleChance)
+	{
+		this.roomPuzzleChance = roomPuzzleChance;
+	}
+
+	public ArrayList<Room> getRoomExits()
+	{
+		return roomExits;
+	}
+	
+	public void setRoomExits(ArrayList<Room> roomExits)
+	{
+		this.roomExits = roomExits;
+	}
+	
+	public String getRoomDescription()
+	{
+		return roomDescription;
+	}
+	
+	public int getRoomMonster()
+	{
+		return roomMonster;
+	}
+	
+	public int getRoomMonsterChance()
+	{
+		return roomMonsterChance;
+	}
+	
+	public int getRoomPuzzle()
+	{
+		return roomPuzzle;
+	}
+	
+	public int getRoomPuzzleChance()
+	{
+		return roomPuzzleChance;
+	}
+}
