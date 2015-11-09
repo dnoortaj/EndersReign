938806cbf883514241de5cbb410d72a0ba8c9c81
Danyelle <Danyelle@10.251.15.94>
2015-11-09 10:33:34 -0500

    it says there are some changes so..PUSH

---
+++ src/UserInteraction/Game.java
@@ -0,0 +1,80 @@
+package UserInteraction;
+
+import Obstacle.*;
+import Inventory.*;
+
+public class Game
+{
+	private int gameStage;
+	private Player gamePlayer;
+	private Room gameCurrentRoom;
+
+	public Game()
+	{
+		gameStage = 0;
+		gamePlayer = new Player();
+		gameCurrentRoom = new Room();
+	}
+	
+	public void startNewGame()
+	{
+		
+	}
+	
+	public void loadGame(int number)
+	{
+		
+	}
+	
+	public void saveGame()
+	{
+		
+	}
+	
+	public void displayHelp()
+	{
+		
+	}
+	
+	public void move()
+	{
+		
+	}
+	
+	public int getGameStage()
+	{
+		return gameStage;
+	}
+
+	public void setGameStage(int gameStage)
+	{
+		this.gameStage = gameStage;
+	}
+
+	public Player getGamePlayer()
+	{
+		return gamePlayer;
+	}
+
+	public void setGamePlayer(Player gamePlayer)
+	{
+		this.gamePlayer = gamePlayer;
+	}
+
+	public Room getGameCurrentRoom()
+	{
+		return gameCurrentRoom;
+	}
+
+	public void setGameCurrentRoom(Room gameCurrentRoom)
+	{
+		this.gameCurrentRoom = gameCurrentRoom;
+	}
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
