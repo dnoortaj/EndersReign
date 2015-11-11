5b1a8b62554fe017d49f6bf27c75f1192cca1e98
Danyelle <Danyelle@10.251.15.94>
2015-11-10 19:51:28 -0500

    new class

---
+++ src/UserInteraction/GameController.java
@@ -0,0 +1,148 @@
+package UserInteraction;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import Inventory.*;
+import Obstacle.*;
+
+public class GameController
+{
+	private ArrayList<Game> games = new ArrayList<Game>();
+	private Game game;
+	private boolean run;
+	
+	public GameController()
+	{
+		games = new ArrayList<Game>();
+		game = new Game();
+		run = false;
+	}
+	
+	public void mainMenu()
+	{		
+		Scanner sc = new Scanner(new InputStreamReader(System.in));
+		try
+		{
+			System.out.println("Welcome to Ender's Reign: Wiggin's Formic Rage!");
+			System.out.println("Which file would you like to open? (Enter a value 1-3)");
+			System.out.print("> ");			
+			loadGame(sc.nextInt());
+			if (run = false)
+			{
+
+			}
+			else
+			{
+				System.out.println("Sorry, you've entered an incorrect value. \nPlease enter a value 1-3.");
+			}
+
+		}
+		catch (Exception e)
+		{
+			run = true;
+			mainMenu();
+		}
+	}
+	
+	public void startNewGame(int number)
+	{
+		readFromFile();
+		
+		
+	}
+	
+	public void loadGame(int number)
+	{
+		readFromFile();
+		game = games.get((number-1));
+		
+	}
+	
+	   public  void readFromFile()
+	    {
+	       ObjectInputStream inFile = null;
+	      
+	       // open the file
+	       try
+	       {
+	           inFile = new ObjectInputStream(new FileInputStream("games.dat"));
+	       }
+	       catch (IOException e)
+	       {
+	           System.out.println("Problem opening games.dat");
+	           e.printStackTrace();
+	          
+	       }
+	
+	       // separate try block to make sure if problem occurs we have a better idea at what point it
+	       // occurs
+	       try
+	       {
+	           while (true)
+	           {
+	               Game g = (Game)inFile.readObject();
+	               games.add(g);
+	           }
+	           
+	       }
+	       catch (EOFException ex)
+	       {
+	           // this is expected -just close file
+	           try
+	        {
+	            inFile.close();
+	        } catch (IOException e)
+	        {
+
+	            System.out.println("Problem closing the file");
+	            e.printStackTrace();
+	        }
+	       }
+	       catch (Exception e)
+	       {
+	           // unexpected exception
+	           System.out.println("Problem reading from file");
+	           e.printStackTrace();
+	       }
+	       
+	       // got all games
+	     
+	    }
+	
+    public void writeToFile(ArrayList<Game> games)
+    {
+        try
+        {
+            ObjectOutputStream outFile = new ObjectOutputStream(new FileOutputStream("games.dat"));
+            
+            // now write the Game classes from the ArrayList
+            for(Game g : games)
+            {
+                outFile.writeObject(g);
+            }
+            outFile.close();
+            
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+            
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+	
+	public static void main(String[] args)
+	{
+		GameController gameCont = new GameController();
+		gameCont.mainMenu();
+	}
+}
